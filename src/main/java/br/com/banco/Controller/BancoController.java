79cb9a1ba8d3b91ad7181dadb509f1ed2d50dfeb
Bruno <brunoferreira.tecnico@gmail.com>
2023-07-14 00:07:14 -0300

    adicionando arquivos de controller, service e repositories

--- src/main/java/br/com/banco/Controller/BancoController.java
+++ src/main/java/br/com/banco/Controller/BancoController.java
@@ -1,4 +1,11 @@
 package br.com.banco.Controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/banco")
 public class BancoController {
 }
---
+++ src/main/java/br/com/banco/Service/BancoService.java
@@ -0,0 +1,7 @@
+package br.com.banco.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BancoService {
+}
