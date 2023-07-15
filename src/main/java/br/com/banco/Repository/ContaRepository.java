259d92ed378a74ae65de65a135c84b2adedcc733
Bruno <brunoferreira.tecnico@gmail.com>
2023-07-15 11:09:15 -0300

    criacao do repository extendendo as classes entity

--- src/main/java/br/com/banco/Repository/ContaRepository.java
+++ src/main/java/br/com/banco/Repository/ContaRepository.java
@@ -1,7 +1,10 @@
 package br.com.banco.Repository;
 
+import br.com.banco.Entity.ContaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ContaRepository {
+public interface ContaRepository extends JpaRepository<ContaEntity, Long> {
+
 }
--- src/main/java/br/com/banco/Repository/TransferenciaRepository.java
+++ src/main/java/br/com/banco/Repository/TransferenciaRepository.java
@@ -1,7 +1,9 @@
 package br.com.banco.Repository;
 
+import br.com.banco.Entity.TransferenciaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransferenciaRepository {
+public interface TransferenciaRepository extends JpaRepository<TransferenciaEntity, Long> {
 }
