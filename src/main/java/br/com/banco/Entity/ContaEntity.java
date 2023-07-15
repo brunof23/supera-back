c730f8a21c9a73fda22f537bf47d0bceeca80c93
Bruno <brunoferreira.tecnico@gmail.com>
2023-07-15 10:59:33 -0300

    mapeando as tabelas conta e transferencia

--- src/main/java/br/com/banco/Entity/ContaEntity.java
+++ src/main/java/br/com/banco/Entity/ContaEntity.java
@@ -1,7 +1,40 @@
 package br.com.banco.Entity;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "conta")
 public class ContaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_conta")
+    private Long id;
+
+    @Column(name = "nome_responsavel", nullable = false, length = 50)
+    private String nomeResponsavel;
+
+    // Construtores, getters e setters
+
+    public ContaEntity() {
+    }
+
+    public ContaEntity(String nomeResponsavel) {
+        this.nomeResponsavel = nomeResponsavel;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeResponsavel() {
+        return nomeResponsavel;
+    }
+
+    public void setNomeResponsavel(String nomeResponsavel) {
+        this.nomeResponsavel = nomeResponsavel;
+    }
 }
--- src/main/java/br/com/banco/Entity/TransferenciaEntity.java
+++ src/main/java/br/com/banco/Entity/TransferenciaEntity.java
@@ -1,7 +1,89 @@
 package br.com.banco.Entity;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "transferencia")
 public class TransferenciaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "data_transferencia", nullable = false)
+    private LocalDateTime dataTransferencia;
+
+    @Column(name = "valor", nullable = false, precision = 20, scale = 2)
+    private BigDecimal valor;
+
+    @Column(name = "tipo", nullable = false, length = 15)
+    private String tipo;
+
+    @Column(name = "nome_operador_transacao", length = 50)
+    private String nomeOperadorTransacao;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "conta_id", nullable = false)
+    private ContaEntity conta;
+
+    public TransferenciaEntity() {
+    }
+
+    public TransferenciaEntity(LocalDateTime dataTransferencia, BigDecimal valor, String tipo, String nomeOperadorTransacao, ContaEntity conta) {
+        this.dataTransferencia = dataTransferencia;
+        this.valor = valor;
+        this.tipo = tipo;
+        this.nomeOperadorTransacao = nomeOperadorTransacao;
+        this.conta = conta;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDataTransferencia() {
+        return dataTransferencia;
+    }
+
+    public void setDataTransferencia(LocalDateTime dataTransferencia) {
+        this.dataTransferencia = dataTransferencia;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNomeOperadorTransacao() {
+        return nomeOperadorTransacao;
+    }
+
+    public void setNomeOperadorTransacao(String nomeOperadorTransacao) {
+        this.nomeOperadorTransacao = nomeOperadorTransacao;
+    }
+
+    public ContaEntity getConta() {
+        return conta;
+    }
+
+    public void setConta(ContaEntity conta) {
+        this.conta = conta;
+    }
 }
