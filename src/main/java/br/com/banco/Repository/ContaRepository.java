b2029a4d8fe59523257b565348f35f8a59b7f74c
Bruno <brunoferreira.tecnico@gmail.com>
2023-07-15 17:00:18 -0300

    refactor ContaRepository

--- src/main/java/br/com/banco/Repository/ContaRepository.java
+++ src/main/java/br/com/banco/Repository/ContaRepository.java
@@ -1,14 +1,10 @@
 package br.com.banco.Repository;
 
 import br.com.banco.Entity.ContaEntity;
-import br.com.banco.Entity.TransferenciaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ContaRepository extends JpaRepository<ContaEntity, Long> {
 
-    List<TransferenciaEntity> findAllById(Long numeroConta);
 }
